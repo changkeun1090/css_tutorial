fbc6f9c261fe8ff5ca105509a80ed39904c0f117
changkeun <changkeun1090@gmail.com>
2023-08-23 14:36:42 +0900

    📝 Managed | 네비게이션 완성

--- manage_landing_page/main.js
+++ manage_landing_page/main.js
@@ -6,6 +6,8 @@ navToggle.addEventListener("click", () => {
 	primaryNav.hasAttribute("data-visible")
 		? navToggle.setAttribute("aria-expanded", false)
 		: navToggle.setAttribute("aria-expanded", true);
-
 	primaryNav.toggleAttribute("data-visible");
+
+	// Drop Shadow
+	primaryHeader.toggleAttribute("data-overlay");
 });
